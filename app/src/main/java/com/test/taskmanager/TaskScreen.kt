39eeea20132e6ff9c251bff308921611d7103860
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-05-19 12:47:33 +0200

    Delete app/src/main/java/com/test/taskmanager/TaskScreen.kt

--- app/src/main/java/com/test/taskmanager/TaskScreen.kt
+++
@@ -1,7 +0,0 @@
-package com.test.taskmanager
-import androidx.compose.runtime.Composable
-@Composable 
-fun TaskScreen(tasks: List<Task>) { 
-    // UI placeholder
-    // TODO: Render tasks list
-}
